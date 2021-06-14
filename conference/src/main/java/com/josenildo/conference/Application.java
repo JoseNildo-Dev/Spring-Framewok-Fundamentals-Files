c17e143ed5c1d304449f518f559200ba328e2356
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2021-06-13 21:26:55 -0300

    feat: Create Main Class
    Main class, starting point of the application, creates an instance of SpeakService and prints the lastName property on the screen

---
+++ conference/src/main/java/com/josenildo/conference/Application.java
@@ -0,0 +1,14 @@
+package com.josenildo.conference;
+
+import com.josenildo.conference.com.josenildo.conference.service.SpeakerService;
+import com.josenildo.conference.com.josenildo.conference.service.SpeakerServiceImpl;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpeakerService service = new SpeakerServiceImpl();
+
+        System.out.println(service.findAll().get(0).getFirstName());
+    }
+}
