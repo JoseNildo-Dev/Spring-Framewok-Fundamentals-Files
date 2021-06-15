cf258dce3ecd95d48b45989659b2faaadbebeb9c
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2021-06-14 21:42:43 -0300

    feat: Convert the application to a spring application
    Removed the old SpeakerService call, which was replaced by a new version, now making use of Spring Framework

--- conference-java/src/main/java/com/josenildo/conference/com/josenildo/conference/Application.java
+++ conference-java/src/main/java/com/josenildo/conference/com/josenildo/conference/Application.java
@@ -2,12 +2,17 @@ package com.josenildo.conference.com.josenildo.conference;
 
 import com.josenildo.conference.com.josenildo.conference.service.SpeakerService;
 import com.josenildo.conference.com.josenildo.conference.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        SpeakerService service = new SpeakerServiceImpl();
+        //SpeakerService service = new SpeakerServiceImpl();
+
+        SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
