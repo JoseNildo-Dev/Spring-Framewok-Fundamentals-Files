1d78c792d6627f4094c2fde4a9b6cec336949286
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2021-06-15 21:27:34 -0300

    feat: Singleton pattern test
    Testing the singleton pattern, creating two different instances and printing them to the screen

--- conference-java/src/main/java/com/josenildo/conference/com/josenildo/conference/AppConfig.java
+++ conference-java/src/main/java/com/josenildo/conference/com/josenildo/conference/AppConfig.java
@@ -4,13 +4,16 @@ import com.josenildo.conference.com.josenildo.conference.repository.HibernateSpe
 import com.josenildo.conference.com.josenildo.conference.repository.SpeakerRepository;
 import com.josenildo.conference.com.josenildo.conference.service.SpeakerService;
 import com.josenildo.conference.com.josenildo.conference.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value=BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
         //service.setRepository(getSpeakerRepository());
